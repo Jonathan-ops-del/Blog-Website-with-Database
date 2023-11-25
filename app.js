36161431c3a7d30462423d32383ee1ebbcb21912
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-11-24 23:29:35 -0500

    updated mongod

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
 
 
 
